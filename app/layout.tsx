49560f3a91bc8875e08a6f55fd278c156b0ed23c
RaduRS <48391644+RaduRS@users.noreply.github.com>
2026-02-03 16:35:35 +0000

    remove a comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -134,7 +134,6 @@ export default async function RootLayout({
             ],
           })}
         </Script>
-        {/* JSON-LD: Website */}
         <Script id="website-jsonld" type="application/ld+json">
           {JSON.stringify({
             "@context": "https://schema.org",
